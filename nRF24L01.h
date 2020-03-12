13b4794e8e226170c50ecf5d204d2ae4e21262f0
danielkluzner <35643045+danielkluzner@users.noreply.github.com>
2020-03-11 21:01:43 -0700

    Update nRF24L01.h

--- nRF24L01.h
+++ nRF24L01.h
@@ -9,12 +9,16 @@
 /* Multiple byte read/write command */ 
 #define MULTIPLEBYTE_CMD        ((uint8_t)0x40)
 
-// PD7 = TRX_CS (Deselect = Chip Select high)
-#define nRF24L01_CSN_LOW   GPIOE->ODR &= ~(1U << 12);
-#define nRF24L01_CSN_HIGH  GPIOE->ODR |=  (1U << 12);
+// PE12 = TRX_CSN (Deselect = Chip Select NOT high)
+#define nRF24L01_CSN_LOW		GPIOE->ODR &= ~(1U << 12);
+#define nRF24L01_CSN_HIGH		GPIOE->ODR |=  (1U << 12);
+
+// PE11 = TRX_CE (Chip enable)
+#define nRF24L01_CE_ON			GPIOE->ODR |=  (1U << 11);
+#define nRF24l01_CE_OFF			GPIOE->ODR &= ~(1U << 11);
 
 void TRX_Init(void);
-void TRX_IO_CS_Init(void);
+void TRX_IO_Init(void);
 void TRX_IO_Write(uint8_t *pBuffer, uint8_t WriteAddr, uint8_t NumByteToWrite);
 void TRX_IO_Read(uint8_t *pBuffer, uint8_t ReadAddr, uint8_t NumByteToRead);
 void nRF24L01_Init(void);
