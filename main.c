ce706b1b74391f05f55da60cde86f455d4cb0ffe
danielkluzner <35643045+danielkluzner@users.noreply.github.com>
2020-03-11 21:00:14 -0700

    dk - add SPI and nRF24L01 initializations

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 
-
 #include "stm32l476xx.h"
 #include "buttonInterrupts.h"
 #include "throttle.h"
 #include "initializations.h"
-
+#include "SysTimer.h"
+#include "nRF24L01.h"
+#include "SPI.h"
 
 
 int main() {
@@ -13,9 +14,10 @@ int main() {
 	joystick_init();
 	interrupt_init();
 	pwm_init();
+	SysTick_Init();
+	TRX_Init();
 	
 	while(1);
 	
 	return 0;
 }
-
