dc6848a5125a010b2be1c65db4cb743f3db5d39c
danielkluzner <35643045+danielkluzner@users.noreply.github.com>
2020-03-17 13:06:17 -0700

    Update nRF24L01.c

--- nRF24L01.c
+++ nRF24L01.c
@@ -10,14 +10,14 @@ void TRX_Init(void) {
 	
 	// Initialize Transceiver
 	TRX_IO_Init();
-	// nRF24L01_Init();
+	nRF24L01_Init();
 }
 
 // Transceiver IO functions
 void TRX_IO_Init(void) {
     
     // E11 = CE
-    // E12 = CS
+    // E12 = CSN
     
     // GPIO output mode for pins 11, 12
 		GPIOE->MODER &= ~GPIO_MODER_MODE11_1;
@@ -43,24 +43,58 @@ void TRX_IO_Init(void) {
     nRF24L01_CE_ON;
 }
 
-void TRX_IO_Write(uint8_t *pBuffer, uint8_t WriteAddr, uint8_t NumByteToWrite){
-	
+void PAYLOAD_Write(uint8_t *pBuffer, uint8_t NumByteToWrite){
 	uint8_t rxBuffer[32];
 	
-	/* Configure the MS bit: 
-       - When 0, the address will remain unchanged in multiple read/write commands.
-       - When 1, the address will be auto incremented in multiple read/write commands.
-	*/
-	if(NumByteToWrite > 0x01){
-		WriteAddr |= (uint8_t) MULTIPLEBYTE_CMD; // Set write mode & auto-increment
-	}
+	uint8_t AddrByte = 0xa0;  // set write mode
 	
 	// Set chip select Low at the start of the transmission 
 	nRF24L01_CSN_LOW;  // TRX CS low
 	SPI_Delay(10);  
 	
 	// Send the Address of the indexed register 
-	SPI_Write(SPI1, &WriteAddr, rxBuffer, 1);
+	SPI_Write(SPI1, &AddrByte, rxBuffer, 1);
+	
+	// Send the data that will be written into the device (MSB First) 
+	SPI_Write(SPI1, pBuffer, rxBuffer, NumByteToWrite);
+  
+	// Set chip select High at the end of the transmission  
+	SPI_Delay(10);
+	nRF24L01_CSN_HIGH; // TRX CSN high
+}
+
+void PAYLOAD_Read(uint8_t *pBuffer, uint8_t NumByteToRead){
+	uint8_t rxBuffer[32];
+	
+	uint8_t AddrByte = 0x61;  // set read mode
+	
+	// Set chip select Low at the start of the transmission 
+	nRF24L01_CSN_LOW; // TRX CS low
+	SPI_Delay(10);
+	
+	// Send the Address of the indexed register 
+	SPI_Write(SPI1, &AddrByte, rxBuffer, 1);
+	
+	// Receive the data that will be read from the device (MSB First) 
+	SPI_Read(SPI1, pBuffer, NumByteToRead);
+  
+	// Set chip select High at the end of the transmission  
+	SPI_Delay(10);
+	nRF24L01_CSN_HIGH; // TRX CSN high
+}
+
+void TRX_IO_Write(uint8_t *pBuffer, uint8_t WriteAddr, uint8_t NumByteToWrite){
+	
+	uint8_t rxBuffer[32];
+	
+	uint8_t AddrByte = WriteAddr | ((uint8_t) 0x20);  // set write mode & autoincrement
+	
+	// Set chip select Low at the start of the transmission 
+	nRF24L01_CSN_LOW;  // TRX CS low
+	SPI_Delay(10);  
+	
+	// Send the Address of the indexed register 
+	SPI_Write(SPI1, &AddrByte, rxBuffer, 1);
 	
 	// Send the data that will be written into the device (MSB First) 
 	SPI_Write(SPI1, pBuffer, rxBuffer, NumByteToWrite);
@@ -74,17 +108,17 @@ void TRX_IO_Read(uint8_t *pBuffer, uint8_t ReadAddr, uint8_t NumByteToRead){
 
 	uint8_t rxBuffer[32];
 	
-	uint8_t AddrByte = ReadAddr | ((uint8_t) 0xC0);  // set read mode & autoincrement
+	uint8_t AddrByte = ReadAddr & ((uint8_t) 0x1f);  // set read mode & autoincrement
 	
 	// Set chip select Low at the start of the transmission 
 	nRF24L01_CSN_LOW; // TRX CS low
 	SPI_Delay(10);
 	
 	// Send the Address of the indexed register 
-	SPI_Write(SPI2, &AddrByte, rxBuffer, 1);
+	SPI_Write(SPI1, &AddrByte, rxBuffer, 1);
 	
 	// Receive the data that will be read from the device (MSB First) 
-	SPI_Read(SPI2, pBuffer, NumByteToRead);
+	SPI_Read(SPI1, pBuffer, NumByteToRead);
   
 	// Set chip select High at the end of the transmission  
 	SPI_Delay(10);
@@ -95,11 +129,13 @@ void TRX_IO_Read(uint8_t *pBuffer, uint8_t ReadAddr, uint8_t NumByteToRead){
 void nRF24L01_Init(void) {
 	// TODO
 	uint8_t tempBuffer;
-	tempBuffer = 0x3F;
-	// bits to write to Gyro CTRL_REG1
-	TRX_IO_Write(&tempBuffer, nRF24L01_CONFIG, 1);
-	tempBuffer = 0x30; // 00110000
-	// bits to write to Gyro CTRL_REG4
-	TRX_IO_Write(&tempBuffer, nRF24L01_CONFIG, 1);
 	
+	
+	
+	// RX mode (PRIM_RX = 1)
+	//tempBuffer = 0x0b; // 00001011
+	// TX mode (PRIM_RX = 0)
+	tempBuffer = 0x0a; // 00001010
+	
+	TRX_IO_Write(&tempBuffer, nRF24L01_CONFIG, 1);	
 }	
