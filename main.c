7afed5dc1d46b33b24f3c6487449c0d886d10f2a
danielkluzner <35643045+danielkluzner@users.noreply.github.com>
2020-03-17 13:08:41 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,20 +4,49 @@
 #include "throttle.h"
 #include "initializations.h"
 #include "SysTimer.h"
+#include "SysClock.h"
 #include "nRF24L01.h"
 #include "SPI.h"
+#include "LCD.h"
+#include <stdio.h>
 
 
 int main() {
 	// Initialization
+	System_Clock_Init();
+	SysTick_Init();
 	clock_inits();
 	joystick_init();
 	interrupt_init();
 	pwm_init();
-	SysTick_Init();
+	LCD_Initialization();
 	TRX_Init();
 	
-	while(1);
+	char RX_Data[8] = {"5"};
+	uint8_t read = 4;
+	uint8_t write = 2;
+	
+	PAYLOAD_Write(&write, 1);
+	
+	while(1){
+		
+		// RECEIVER (CHRIS'S BOARD)
+		LCD_DisplayString((uint8_t*) RX_Data);
+		for(int i = 0; i < 10000000; i++);
+		sprintf(RX_Data, "%d", read);
+		LCD_DisplayString((uint8_t*) RX_Data);
+		for(int i = 0; i < 10000000; i++);
+		PAYLOAD_Read(&read, 1);
+		sprintf(RX_Data, "%d", read);
+		LCD_DisplayString((uint8_t*) RX_Data);
+		for(int i = 0; i < 10000000; i++);
+		
+		
+		
+		// TRANSMITTER (DANIEL'S BOARD)
+
+		
+	}
 	
 	return 0;
 }
