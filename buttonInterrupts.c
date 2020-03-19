8a6bf9d38cc2ad9727ff180911fba708171e581b
danielkluzner <35643045+danielkluzner@users.noreply.github.com>
2020-03-18 18:42:25 -0700

    Update buttonInterrupts.c

--- buttonInterrupts.c
+++ buttonInterrupts.c
@@ -1,7 +1,9 @@
 
 #include "buttonInterrupts.h"
 #include "throttle.h"
-
+#include "nRF24L01.h"
+#include "LCD.h"
+#include <stdio.h>
 
 
 //void EXTI0_IRQHandler(void){
@@ -17,11 +19,20 @@
 //	Green_LED_Toggle();
 //}
 
-//void EXTI2_IRQHandler(void){
-//	EXTI->PR1 |= EXTI_PR1_PIF2;
-//	
-//	midThrottle();
-//}
+void EXTI2_IRQHandler(void){
+	
+	
+	char RX_Data[1];
+	uint8_t read;
+	
+	TRX_IO_Read(&read, nRF24L01_STATUS, 1);
+	sprintf(RX_Data, "%x", read);
+	LCD_DisplayString(RX_Data);
+	uint8_t temp = 0x70;
+	TRX_IO_Write(&temp, nRF24L01_STATUS, 1);
+	
+	EXTI->PR1 |= EXTI_PR1_PIF2;
+}
 
 //PA3, up
 void EXTI3_IRQHandler(void){
